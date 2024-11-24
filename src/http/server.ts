a524a1033ff0c72fc692c3b834ea3f3954596fc9
Herley Oliveira <herley.oliveira@bancoartico.com>
2024-11-24 17:42:33 -0300

    --amend

--- src/http/server.ts
+++ src/http/server.ts
@@ -12,6 +12,20 @@ import {
   productsIdRoute,
   updateProductRoute,
 } from './routes/products'
+import {
+  createOrderRoute,
+  deleteOrderRoute,
+  getAllOrdersRoute,
+  getOrderByIdRoute,
+  updateOrderRoute,
+} from './routes/orders'
+import {
+  createSaleRoute,
+  deleteSaleRoute,
+  getAllSalesRoute,
+  getSaleByIdRoute,
+  updateSaleRoute,
+} from './routes/sales'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -22,11 +36,26 @@ app.register(fastifyCors, {
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+//* Rotas de produtos
 app.register(createProductRoute)
 app.register(productsAllRoute)
 app.register(productsIdRoute)
 app.register(updateProductRoute)
 app.register(deleteProductRoute)
+
+//* Rotas de pedidos
+app.register(createOrderRoute)
+app.register(getAllOrdersRoute)
+app.register(getOrderByIdRoute)
+app.register(updateOrderRoute)
+app.register(deleteOrderRoute)
+
+//* Rotas de vendas
+app.register(createSaleRoute)
+app.register(getAllSalesRoute)
+app.register(getSaleByIdRoute)
+app.register(updateSaleRoute)
+app.register(deleteSaleRoute)
 
 app.listen({ port: 3004 }).then(() => {
   console.log('HTTP server running')
